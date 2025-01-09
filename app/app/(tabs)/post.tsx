b6567c7de466c6c40840e84bdb27812fb036f7fa
dammar093 <dammarrana093@gmail.com>
2025-01-09 21:37:54 +0545

    add input field

--- app/app/(tabs)/post.tsx
+++ app/app/(tabs)/post.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import color from "@/constants/Colors";
 import {
   StyleSheet,
@@ -8,56 +8,228 @@ import {
   StatusBar,
   Image,
   TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
+import CustomInput from "@/components/custiomInput/CustomInput";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import iconsizes from "@/constants/IconSizes";
 
 const Post = () => {
   const [images, setImages] = useState<string[]>([]);
-  const [formValues, setFormValues] = useState({
+  const [services, setServices] = useState<string[]>([]);
+  const [newService, setNewService] = useState<string>("");
+  const [formValues, setFormValues] = useState<{
+    images: string[];
+    title: string;
+    services: string[];
+    description: string;
+    location: string;
+    longitude: number | null;
+    latitude: number | null;
+    price: number | null;
+  }>({
     images: [],
     title: "",
     services: [],
     description: "",
+    price: null,
     location: "",
     longitude: null,
     latitude: null,
   });
 
+  useEffect(() => {
+    const getLocation = async () => {
+      let { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setFormValues((prevValues) => ({
+        ...prevValues,
+        longitude: location.coords.longitude,
+        latitude: location.coords.latitude,
+      }));
+    };
+
+    getLocation();
+  }, []);
+
   const pickImages = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: "images",
       allowsMultipleSelection: true,
-      allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
-
     if (!result.canceled && result.assets && result.assets.length > 0) {
       const uris = result.assets.map((asset) => asset.uri);
-      setImages(uris);
+      setImages((prevImages) => [...prevImages, ...uris]);
+      setFormValues((prevValues) => ({
+        ...prevValues,
+        images: [...prevValues.images, ...uris],
+      }));
     }
+  };
+
+  const removeImage = (index: number) => {
+    const newImages = images.filter((_, i) => i !== index);
+    setImages(newImages);
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      images: newImages,
+    }));
+  };
+
+  const addService = () => {
+    if (newService.trim() !== "") {
+      setServices((prevServices) => [...prevServices, newService]);
+      setFormValues((prevValues) => ({
+        ...prevValues,
+        services: [...prevValues.services, newService],
+      }));
+      setNewService("");
+    }
+  };
+
+  const removeService = (index: number) => {
+    const newServices = services.filter((_, i) => i !== index);
+    setServices(newServices);
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      services: newServices,
+    }));
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View style={styles.inputContainer}>
-          <TouchableOpacity onPress={pickImages} style={styles.imageContainer}>
-            {images.length > 0 ? (
-              images.map((image, index) => (
-                <Image
-                  key={index}
-                  source={{ uri: image }}
-                  style={styles.image}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <ScrollView
+          contentContainerStyle={{
+            paddingBottom: 50,
+          }}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={styles.inputContainer}>
+            <View style={styles.imageGrid}>
+              {images.map((image, index) => (
+                <View key={index} style={styles.imageWrapper}>
+                  <Image source={{ uri: image }} style={styles.image} />
+                  <TouchableOpacity
+                    style={styles.removeButton}
+                    onPress={() => removeImage(index)}
+                  >
+                    <Ionicons name="close-circle" size={24} color="red" />
+                  </TouchableOpacity>
+                </View>
+              ))}
+              <TouchableOpacity
+                onPress={pickImages}
+                style={styles.addImageButton}
+              >
+                <Feather
+                  name="plus"
+                  size={iconsizes.xl}
+                  color={color.lightBlack}
                 />
-              ))
-            ) : (
-              <Text style={styles.imagePlaceholder}>Tap to select images</Text>
-            )}
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+                <Text style={styles.addImageText}>Add Images</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={{ gap: 20 }}>
+              <View>
+                <CustomInput
+                  onChangeText={(text) =>
+                    setFormValues((prevValues) => ({
+                      ...prevValues,
+                      title: text,
+                    }))
+                  }
+                  placeholder="Enter title"
+                  value={formValues?.title}
+                  secureTextEntry={false}
+                />
+              </View>
+              <View>
+                <CustomInput
+                  onChangeText={(text) =>
+                    setFormValues((prevValues) => ({
+                      ...prevValues,
+                      price: parseFloat(text),
+                    }))
+                  }
+                  placeholder="Enter price"
+                  value={formValues?.price?.toString()}
+                  secureTextEntry={false}
+                  type="number-pad"
+                />
+              </View>
+              <View style={styles.serviceContainer}>
+                {services.map((service, index) => (
+                  <View key={index} style={styles.serviceWrapper}>
+                    <Text style={styles.serviceText}>{service}</Text>
+                    <TouchableOpacity
+                      style={styles.removeButton}
+                      onPress={() => removeService(index)}
+                    >
+                      <Ionicons name="close-circle" size={24} color="red" />
+                    </TouchableOpacity>
+                  </View>
+                ))}
+                <View style={styles.addServiceContainer}>
+                  <CustomInput
+                    inputStyle={styles.serviceInput}
+                    placeholder="Enter service"
+                    value={newService}
+                    onChangeText={setNewService}
+                  />
+                  <TouchableOpacity
+                    onPress={addService}
+                    style={styles.addServiceButton}
+                  >
+                    <Feather
+                      name="plus"
+                      size={iconsizes.lg}
+                      color={color.white}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View>
+                <CustomInput
+                  onChangeText={() => {}}
+                  placeholder="Enter address"
+                  value={formValues?.description}
+                />
+              </View>
+              <View>
+                <TextInput
+                  style={styles.description}
+                  onChangeText={(text) =>
+                    setFormValues((prevValues) => ({
+                      ...prevValues,
+                      description: text,
+                    }))
+                  }
+                  placeholder="Enter description"
+                  placeholderTextColor={color.lightBlack}
+                  value={formValues?.description}
+                  multiline
+                />
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
       <StatusBar barStyle="dark-content" />
     </SafeAreaView>
   );
@@ -72,37 +244,78 @@ const styles = StyleSheet.create({
   inputContainer: {
     marginBottom: 16,
   },
-  label: {
-    fontSize: 16,
-    marginBottom: 8,
+  imageGrid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
   },
-  input: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 4,
-    padding: 8,
-    fontSize: 16,
-  },
-  textArea: {
-    height: 100,
-  },
-  imageContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 200,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 4,
+  imageWrapper: {
+    position: "relative",
+    width: "48%",
     marginBottom: 16,
   },
   image: {
     width: "100%",
-    height: "100%",
+    height: 200,
     borderRadius: 4,
   },
-  imagePlaceholder: {
+  removeButton: {
+    position: "absolute",
+    top: 8,
+    right: 8,
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    borderRadius: 12,
+  },
+  addImageButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "48%",
+    height: 200,
+    borderWidth: 1,
+    borderColor: color.lightBlack,
+    borderRadius: 10,
+    marginBottom: 16,
+  },
+  addImageText: {
     fontSize: 16,
-    color: "#aaa",
+    color: color.lightBlack,
+  },
+  serviceContainer: {
+    marginTop: 20,
+  },
+  serviceWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  serviceText: {
+    flex: 1,
+    fontSize: 16,
+    backgroundColor: color.garyWhite,
+    padding: 10,
+    borderRadius: 10,
+  },
+  addServiceContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  serviceInput: {
+    flex: 1,
+    borderColor: color.lightBlack,
+    borderRadius: 4,
+    padding: 10,
+    marginRight: 10,
+  },
+  addServiceButton: {
+    padding: 10,
+    backgroundColor: color.primary,
+    borderRadius: "50%",
+  },
+  description: {
+    minHeight: 300,
+    backgroundColor: "#F0F2F5",
+    borderRadius: 10,
+    paddingHorizontal: 20,
   },
 });
 
--- app/components/custiomInput/CustomInput.tsx
+++ app/components/custiomInput/CustomInput.tsx
@@ -1,37 +1,42 @@
-import { View, TextInput, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Pressable,
+  KeyboardTypeOptions,
+} from "react-native";
 import React, { FC, useState } from "react";
 import color from "@/constants/Colors";
 import Entypo from "@expo/vector-icons/Entypo";
 import iconsizes from "@/constants/IconSizes";
 
+type Values = string | number | any;
 interface inputProps {
   onChangeText: (value: string) => void;
   placeholder: string;
   icon?: React.ReactNode;
   inputStyle?: any;
-  value: string;
+  value: Values;
   secureTextEntry?: boolean;
+  type?: KeyboardTypeOptions;
 }
 
-const CustomInput: FC<inputProps> = ({
-  onChangeText,
-  value,
-  icon,
-  inputStyle,
-  secureTextEntry,
-  placeholder,
-}) => {
+const CustomInput: FC<inputProps> = (
+  { onChangeText, value, icon, inputStyle, secureTextEntry, placeholder, type },
+  props
+) => {
   const [hidePassword, setHidePassword] = useState(secureTextEntry);
   return (
     <View style={[styles.inputContainer, inputStyle]}>
       {icon}
       <TextInput
-        style={{ width: "80%", paddingHorizontal: 10 }}
+        style={[{ width: "80%", paddingHorizontal: 10 }, inputStyle]}
         secureTextEntry={hidePassword}
         placeholder={placeholder}
         value={value}
         onChangeText={onChangeText}
         placeholderTextColor={color.lightBlack}
+        keyboardType={type ? type : "default"}
       />
       {secureTextEntry && (
         <Pressable
@@ -60,12 +65,12 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     width: "100%",
     height: 60,
-    borderWidth: 1,
     flexDirection: "row",
     paddingHorizontal: 20,
     alignItems: "center",
     borderRadius: 10,
     gap: 5,
     position: "relative",
+    backgroundColor: "#F0F2F5",
   },
 });
