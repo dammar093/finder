d1ecfd23bbc02270ad3c6f8706dacf478e8f4d26
dammar093 <dammarrana093@gmail.com>
2025-01-09 20:16:09 +0545

    add expo image picker

--- app/app/(tabs)/post.tsx
+++ app/app/(tabs)/post.tsx
@@ -1,8 +1,109 @@
-import { View, Text, SafeAreaView, FlatList } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import color from "@/constants/Colors";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as ImagePicker from "expo-image-picker";
 
 const Post = () => {
-  return <SafeAreaView></SafeAreaView>;
+  const [images, setImages] = useState<string[]>([]);
+  const [formValues, setFormValues] = useState({
+    images: [],
+    title: "",
+    services: [],
+    description: "",
+    location: "",
+    longitude: null,
+    latitude: null,
+  });
+
+  const pickImages = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: "images",
+      allowsMultipleSelection: true,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      const uris = result.assets.map((asset) => asset.uri);
+      setImages(uris);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.inputContainer}>
+          <TouchableOpacity onPress={pickImages} style={styles.imageContainer}>
+            {images.length > 0 ? (
+              images.map((image, index) => (
+                <Image
+                  key={index}
+                  source={{ uri: image }}
+                  style={styles.image}
+                />
+              ))
+            ) : (
+              <Text style={styles.imagePlaceholder}>Tap to select images</Text>
+            )}
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      <StatusBar barStyle="dark-content" />
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: color.white,
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    padding: 8,
+    fontSize: 16,
+  },
+  textArea: {
+    height: 100,
+  },
+  imageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 200,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    marginBottom: 16,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 4,
+  },
+  imagePlaceholder: {
+    fontSize: 16,
+    color: "#aaa",
+  },
+});
 
 export default Post;
